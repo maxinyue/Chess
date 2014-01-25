82273abdd1671760763e87f89f34304433191c39
Obama <240817532@qq.com>
2014-01-25 11:42:17 +0800

    1

--- js/chess.js
+++ js/chess.js
@@ -2,29 +2,53 @@ var chessboard = {
     horizonNumber: 9,
     verticalNumber: 5,
     spacing: 50,
-    padding: 30,
+    padding: 40,
     x: 80,
     y: 80
 };
 
-chessboard.middleXY = ((chessboard.lineNumber + 1) / 2 - 1) * chessboard.spacing;
 var pieces = [
-    {text: '车', color: 'red'},
-    {text: '马', color: 'red'},
-    {text: '炮', color: 'red'},
-    {text: '将', color: 'red'},
-    {text: '象', color: 'red'},
-    {text: '士', color: 'red'},
-    {text: '卒', color: 'red'},
-    {text: '车', color: 'black'},
-    {text: '马', color: 'black'},
-    {text: '炮', color: 'black'},
-    {text: '帅', color: 'black'},
-    {text: '相', color: 'black'},
-    {text: '士', color: 'black'},
-    {text: '兵', color: 'black'}
+    {text: '将', color: 'red', value: 6},
+    {text: '士', color: 'red', value: 5},
+    {text: '士', color: 'red', value: 5},
+    {text: '象', color: 'red', value: 4},
+    {text: '象', color: 'red', value: 4},
+    {text: '马', color: 'red', value: 3},
+    {text: '马', color: 'red', value: 3},
+    {text: '车', color: 'red', value: 2},
+    {text: '车', color: 'red', value: 2},
+    {text: '炮', color: 'red', value: 1},
+    {text: '炮', color: 'red', value: 1},
+    {text: '卒', color: 'red', value: 0},
+    {text: '卒', color: 'red', value: 0},
+    {text: '卒', color: 'red', value: 0},
+    {text: '卒', color: 'red', value: 0},
+    {text: '卒', color: 'red', value: 0},
+    {text: '帅', color: 'black', value: 6},
+    {text: '士', color: 'black', value: 5},
+    {text: '士', color: 'black', value: 5},
+    {text: '相', color: 'black', value: 4},
+    {text: '相', color: 'black', value: 4},
+    {text: '马', color: 'black', value: 3},
+    {text: '马', color: 'black', value: 3},
+    {text: '车', color: 'black', value: 2},
+    {text: '车', color: 'black', value: 2},
+    {text: '炮', color: 'black', value: 1},
+    {text: '炮', color: 'black', value: 1},
+    {text: '兵', color: 'black', value: 0},
+    {text: '兵', color: 'black', value: 0},
+    {text: '兵', color: 'black', value: 0},
+    {text: '兵', color: 'black', value: 0},
+    {text: '兵', color: 'black', value: 0}
 ];
 var positions = [];
+
+var status = {
+    current: 'choose_side',
+    list:['choose_side','red_turn', 'black_turn']
+};
+
+var my_turn=true;
 
 var initPositions = function () {
     var i;
@@ -33,8 +57,8 @@ var initPositions = function () {
     var length2 = chessboard.verticalNumber - 1;
     for (i = 0; i < length1; i++) {
         for (j = 0; j < length2; j++) {
-            positions.push({x: (i + 1) * chessboard.spacing + chessboard.x, y: (j + 1) * chessboard.spacing + chessboard.y});
-            console.log("x=" + ((i + 1) * chessboard.spacing + chessboard.x ) + " y=" + ((j + 1) * chessboard.spacing + chessboard.y));
+            positions.push({indexX: i, indexY: j, x: (i + 1 / 2) * chessboard.spacing + chessboard.padding + chessboard.x, y: (j + 1 / 2) * chessboard.spacing + chessboard.padding + chessboard.y});
+            //      console.log("x=" + ((i + 1) * chessboard.spacing + chessboard.x ) + " y=" + ((j + 1) * chessboard.spacing + chessboard.y));
         }
     }
 };
@@ -74,34 +98,57 @@ var disorder = function (array) {
 };
 
 var drawPiece = function (color, text, x, y) {
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = 'red';
-    ctx.strokeStyle = 'red';
-    ctx.font = chessboard.padding + 'px Arial';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillText(text, x, y);
-    ctx.beginPath();
-    ctx.arc(x, y, chessboard.padding / 2, 0, 2 * Math.PI);
-    ctx.stroke();
+    drawBlankPiece(x, y);
+    drawCharacter(color, text, x, y);
 };
 
 var randomPositions = function () {
     disorder(positions);
 };
 
-var initPieces = function () {
+var randomPieces = function () {
+    var i;
+    var length = pieces.length;
+    for (i = 0; i < length; i++) {
+        pieces[i].position = positions[i];
+    }
+};
 
+var drawBlankPieces = function () {
+    var i;
+    var length = positions.length;
+    for (i = 0; i < length; i++) {
+        drawBlankPiece(positions[i].x, positions[i].y);
+    }
+};
+
+var drawBlankPiece = function (x, y) {
+    var ctx = canvas.getContext("2d");
+    ctx.strokeStyle = 'brown';
+    ctx.beginPath();
+    ctx.arc(x, y, chessboard.padding / 2, 0, 2 * Math.PI);
+    ctx.stroke();
+};
+
+var drawCharacter = function (color, text, x, y) {
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = 'red';
+    ctx.font = chessboard.padding + 'px Arial';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(text, x, y);
 };
 
 var init = function () {
     drawChessboard();
     initPositions();
     randomPositions();
+    randomPieces();
+    drawBlankPieces();
+    status.current='choose_side'
 };
 
 init();
-
 
 canvas.addEventListener('mousedown', function (e) {
     console.log("layerX" + e.layerX + " layerY" + e.layerY);
